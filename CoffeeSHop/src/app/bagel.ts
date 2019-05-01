b4bbd1b2d11208656f17e1cca116d02f297a2471
Jared Hudson <jared.hudson@daktronics.com>
2019-05-01 15:58:30 -0500

    added tip and total cost calculator to checkout page

--- CoffeeSHop/src/app/bagel.ts
+++ CoffeeSHop/src/app/bagel.ts
@@ -11,7 +11,20 @@ export class Bagel {
   butter = false;
   price: number;
 
-  getTotalPrice() {
-
+  getTotalPrice(): number {
+    let totalPrice = this.price;
+    if(this.creamCheese){
+      totalPrice += .50;
+    }
+    else if(this.grapeJelly){
+      totalPrice += .75;
+    }
+    else if(this.appleJelly){
+      totalPrice += .75;
+    }
+    else if(this.price){
+      totalPrice += .50;
+    }
+    return totalPrice;
   }
 }
--- CoffeeSHop/src/app/checkout/checkout.component.ts
+++ CoffeeSHop/src/app/checkout/checkout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Bagel } from '../bagel';
 import { DataSingleton } from '../data-singleton';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
@@ -9,9 +10,33 @@ import { DataSingleton } from '../data-singleton';
 })
 export class CheckoutComponent implements OnInit {
   bagels: Bagel[];
+  total: number;
+  tip: number;
+  viewableTip: string;
+  totalWithTip: string;
   
-  constructor() { 
+  constructor(private router: Router) { 
     this.bagels = DataSingleton.cart;
+    this.total = 0;
+    let i = 0;
+    for(i = 0; i < this.bagels.length; i++){
+      this.total += this.bagels[i].getTotalPrice();
+      console.log(this.bagels[i].getTotalPrice());
+      console.log(this.total);
+    }
+    this.tip = 0;
+    this.totalWithTip = this.total.toFixed(2);
+  }
+
+  calcTip(amount){
+    this.tip = this.total * amount;
+    this.viewableTip = this.tip.toFixed(2);
+    this.totalWithTip = (this.total + this.tip).toFixed(2);
+  }
+
+  checkout(){
+    DataSingleton.cart = Array<Bagel>();
+    this.router.navigate(['/success-screen']);
   }
 
   ngOnInit() {
